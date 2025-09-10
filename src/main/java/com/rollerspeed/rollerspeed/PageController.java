d13ed5c86b9f63e5ca7d2a6c0dd513e8d19f6dae
Miguelit000 <mastergrisf@gmail.com>
2025-09-09 19:43:13 -0500

    Se agrego el inicio de sesion

--- src/main/java/com/rollerspeed/rollerspeed/PageController.java
+++ src/main/java/com/rollerspeed/rollerspeed/PageController.java
@@ -2,13 +2,12 @@ package com.rollerspeed.rollerspeed;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import com.rollerspeed.rollerspeed.models.User;
 import com.rollerspeed.rollerspeed.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -16,10 +15,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class PageController {
 
+    private final RollerspeedApplication rollerspeedApplication;
+
     private final UserService userService;
 
-    PageController(UserService userService) {
+    PageController(UserService userService, RollerspeedApplication rollerspeedApplication) {
         this.userService = userService;
+        this.rollerspeedApplication = rollerspeedApplication;
     }
 
     @GetMapping("/")
@@ -63,6 +65,20 @@ public class PageController {
     @PostMapping("/registro")
     public String processRegistration(@ModelAttribute User user) {
         userService.savUser(user);
+        return "redirect:/";
+    }
+
+    // Metodo para mostrar la pagina de inicio de sesion
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    // Metodo para procesar el incio de sesion (simulado)
+    @PostMapping("/login")
+    public String processLogin() {
+        // En este paso, simplemente redirigimos.
+        //Despues, agregamos la logica real de la validacion de usuario
         return "redirect:/";
     }
         
